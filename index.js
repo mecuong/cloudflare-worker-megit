bc4f24df91571ef9d43bd2d911ff4af617ba9a86
Cuong Nguyen <nguyen.cuong@marketenterprise.vn>
2022-03-18 20:57:46 +0700

    slack message core

--- index.js
+++ index.js
@@ -6,87 +6,71 @@ addEventListener('fetch', event => {
  * Respond with hello worker text
  * @param {Request} request
  */
- async function handleRequest(request, event) {
-  event.waitUntil(updateProject());
-  return new Response('OK', {
-    headers: { 'content-type': 'text/plain' }
+async function handleRequest(request, event) {
+  const res = await postMessage(request);
+
+  return new Response(JSON.stringify(res), {
+    headers: { 'content-type': 'application/json' }
   })
 }
 
-addEventListener("scheduled", event => {
-  event.waitUntil(updateProject())
-})
+async function postMessage(request) {
+  const body = await request.json();
 
-async function updateProject() {
-  const project = await getLastAutoProject();
-  const body = await createProjectBody(project);
+  if (!body.text || !body.email) {
+    return {
+      text: 'Missing text or email',
+      response_type: 'ephemeral'
+    }
+  }
 
-  await gitFetcher('/projects/' + project.id, {
-    method: 'PATCH',
+  const user = await MEMBERS.get(body.email);
+  if (!user) {
+    await getUsers(request);
+  }
+
+  const userObj = JSON.parse(user);
+
+  // slack post message
+  const res = await gitFetcher('/api/chat.postMessage', {
     body: {
-      project_id: project.id,
-      body,
+      channel: userObj.id,
+      text: body.text,
+      as_user: true
+    },
+    method: 'POST',
+    headers: {
+      'Authorization': request.headers.get('Authorization')
     }
   });
-}
 
-async function getLastAutoProject()
-{
-  const data = await gitFetcher(`/repos/${ GITHUB_USER }/${ GITHUB_REPO }/projects`, {
-    body: {
-      per_page: 100
+  if (!res.ok) {
+    return {
+      text: res.error,
+      response_type: 'ephemeral'
     }
-  });
+  }
 
-  const autoOpenProjects = data.filter(project => project.name.endsWith(AUTO_SIGN) && project.state === 'open');
-
-  // Return last auto project id
-  return autoOpenProjects.pop();
+  return {
+    text: 'Message sent',
+    response_type: 'ephemeral'
+  }
 }
 
-async function createProjectBody(project) {
-  const projectColumns = await getProjectColumns(project.id);
-
-  const cards = await Promise.all(projectColumns.map(projectColumn => {
-    return gitFetcher(projectColumn.cards_url);
-  })).then(projectCards => {
-    return projectCards.flat();
-  });
-
-  const issues = cards.filter(card =>  card.content_url && card.content_url.includes('/issues/'))
-  .map(card => {
-    const status = projectColumns.find(projectColumn => projectColumn.url === card.column_url).name;
-    const name = `#${card.content_url.split('/').pop()}`;
-      return {
-        name,
-        status,
-      };
-  });
-
-  let body = project.body;
-
-  project.body.split('\n').forEach(line => {
-    issues.forEach(issue => {
-      if (line.includes(`[${issue.name}]`)) {
-        let newLine = line.replace(/\|[^\|]*\|[^|]*$/, `| ${issue.status} |\n`);
-        body = body.replace(line, newLine);
-      }
-    });
-  });
-
-  return body;
-}
-
-async function getProjectColumns(project_id) {
-  return await gitFetcher(`/projects/${project_id}/columns`, {
-    body: {
-      project_id,
+async function getUsers(request) {
+  const users = await gitFetcher('/api/users.list', {
+    headers: {
+      'Authorization': request.headers.get('Authorization')
     }
-  });
+  })
+  for (const user of users.members) {
+    // Set to KV namesapce MEMBERS
+    await MEMBERS.put(user.profile.email || user.name, JSON.stringify(user));
+  }
 }
 
 async function gitFetcher(url, options = {}) {
-  const baseUrl = 'https://api.github.com';
+  const baseUrl = 'https://slack.com';
 
   if (url.startsWith('/')) {
     url = new URL(url, baseUrl);
@@ -94,12 +78,10 @@ async function gitFetcher(url, options = {}) {
     url = new URL(url);
   }
 
-  options.headers = {
-    'User-Agent': 'me-git',
+  options.headers = Object.assign(options.headers, {
+    'User-Agent': 'me-slack',
     'Content-Type': 'application/json; charset=utf-8',
-    'Accept': 'application/vnd.github.v3+json',
-    'Authorization': 'Bearer ' + GITHUB_TOKEN
-  }
+  });
 
   options.method = options.method || 'GET';
 
